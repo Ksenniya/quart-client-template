9e75b388b55ad2cf3d99b057922b97fac110e047
Ksenniya <lukkonina1@gmail.com>
2025-02-04 13:33:28 -0300

    Removed api registration template: 57a368ca-e312-11ef-b0dc-40c2ba0ac9eb

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
 from entity.data_ingestion_job.api import api_bp_data_ingestion_job
-from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
+
 
 logging.basicConfig(level=logging.INFO)
 
 app = Quart(__name__)
 QuartSchema(app)
 app.register_blueprint(api_bp_data_ingestion_job, url_prefix='/api/data_ingestion_job')
-app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
+
 
 @app.before_serving
 async def startup():
