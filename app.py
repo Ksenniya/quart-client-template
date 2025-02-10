4bc45c8a1463053e2fdb8322080282a8b782ad30
Ksenniya <lukkonina1@gmail.com>
2025-02-10 18:12:49 -0300

    Added api registration: 3569b235-e7f3-11ef-967a-40c2ba0ac9eb

--- app.py
+++ app.py
@@ -7,12 +7,18 @@ from common.grpc_client.grpc_client import grpc_stream
 from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
+from entity.analyze_data.api import api_bp_analyze_data
+from entity.save_report.api import api_bp_save_report
+from entity.download_data.api import api_bp_download_data
 from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
 
 logging.basicConfig(level=logging.INFO)
 
 app = Quart(__name__)
 QuartSchema(app)
+app.register_blueprint(api_bp_analyze_data, url_prefix='/api/analyze_data')
+app.register_blueprint(api_bp_save_report, url_prefix='/api/save_report')
+app.register_blueprint(api_bp_download_data, url_prefix='/api/download_data')
 app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
 
 @app.before_serving
