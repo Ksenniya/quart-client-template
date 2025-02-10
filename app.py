9b2d378cc4f828dba9a76de8c4cc671409a59af8
Ksenniya <lukkonina1@gmail.com>
2025-02-10 18:34:43 -0300

    Removed api registration template: 7b911c81-e7f4-11ef-ab39-40c2ba0ac9eb

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from app_init.app_init import cyoda_token
 #please update this line to your entity
 from entity.generate_report.api import api_bp_generate_report
 from entity.get_report.api import api_bp_get_report
-from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -17,7 +17,7 @@ app = Quart(__name__)
 QuartSchema(app)
 app.register_blueprint(api_bp_generate_report, url_prefix='/api/generate_report')
 app.register_blueprint(api_bp_get_report, url_prefix='/api/get_report')
-app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
+
 
 @app.before_serving
 async def startup():
