ec89609485a7cb4044e2e97f74c9ffb018d8bc55
Ksenniya <lukkonina1@gmail.com>
2025-02-11 07:51:32 -0300

    Added api registration: 601e4cc4-e865-11ef-9020-40c2ba0ac9eb

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ from common.grpc_client.grpc_client import grpc_stream
 from common.repository.cyoda.cyoda_init import init_cyoda
 from app_init.app_init import cyoda_token
 #please update this line to your entity
+from entity.environments.api import api_bp_environments
+from entity.user_applications.api import api_bp_user_applications
 from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
 
 logging.basicConfig(level=logging.INFO)
 
 app = Quart(__name__)
 QuartSchema(app)
+app.register_blueprint(api_bp_environments, url_prefix='/api/environments')
+app.register_blueprint(api_bp_user_applications, url_prefix='/api/user_applications')
 app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
 
 @app.before_serving
