0bcedfc21807961fd5619e87ea7af606844426d6
Ksenniya <lukkonina1@gmail.com>
2025-02-19 10:10:38 -0300

    saved entity/environment/api.py: 6c8ffcf9-eec2-11ef-a2e6-40c2ba0ac9eb

---
+++ entity/environment/api.py
@@ -0,0 +1,53 @@
+from quart import Blueprint, request, jsonify
+from app_init.app_init import entity_service, cyoda_token
+from common.config.config import ENTITY_VERSION
+import uuid
+import asyncio
+
+api_bp_environment = Blueprint('api/environment', __name__)
+
+ENTITY_MODEL = 'environment'
+
+@api_bp_environment.route('/environments', methods=['POST'])
+async def add_environment():
+    """Create a new environment."""
+    data = await request.json
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    try:
+        environment_id = await entity_service.add_item(
+            token=cyoda_token,
+            entity_model=ENTITY_MODEL,
+            entity_version=ENTITY_VERSION,
+            entity=data
+        )
+        return jsonify({'environment_id': environment_id}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@api_bp_environment.route('/environment/<id>', methods=['GET'])
+async def get_id(id):
+    """Retrieve a environment by ID."""
+    try:
+        data = await entity_service.get_item(
+            token=cyoda_token,
+            entity_model=ENTITY_MODEL,
+            entity_version=ENTITY_VERSION,
+            technical_id=id
+        )
+        return jsonify({"data": data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@api_bp_environment.route('/environments', methods=['GET'])
+async def get_environments():
+    """Retrieve all environments entries."""
+    try:
+        data = await entity_service.get_items(
+            token=cyoda_token,
+            entity_model=ENTITY_MODEL,
+            entity_version=ENTITY_VERSION
+        )
+        return jsonify({"data": data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
