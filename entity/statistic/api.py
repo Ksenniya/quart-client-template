1173b6c620039ab8c653938386aca8486c9a654a
Ksenniya <lukkonina1@gmail.com>
2025-02-19 09:59:21 -0300

    saved entity/statistic/api.py: e9f20a48-eec0-11ef-b80d-40c2ba0ac9eb

---
+++ entity/statistic/api.py
@@ -0,0 +1,23 @@
+from quart import Blueprint, request, jsonify
+from app_init.app_init import entity_service, cyoda_token
+from common.config.config import ENTITY_VERSION
+import uuid
+import asyncio
+
+api_bp_statistic = Blueprint('api/statistic', __name__)
+
+ENTITY_MODEL = 'statistic'
+
+@api_bp_statistic.route('/deploy/environments/<id>/statistics', methods=['GET'])
+async def get_id(id):
+    """Retrieve statistic information."""
+    try:
+        data = await entity_service.get_item(
+            token=cyoda_token,
+            entity_model=ENTITY_MODEL,
+            entity_version=ENTITY_VERSION,
+            technical_id=id
+        )
+        return jsonify({"data": data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
