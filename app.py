1de371d1ed0b4f2f39135cf13f52fa6f489ee6b7
Ksenniya <lukkonina1@gmail.com>
2025-03-07 12:51:59 -0300

    fixed

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-import httpx
+import asyncio
+import logging
 
-@app.route('/store/inventory', methods=['GET'])
-async def get_inventory():
-    async with httpx.AsyncClient() as client:
-        response = await client.get('https://petstore.swagger.io/v2/store/inventory')
-        return response.json(), response.status_code
+from quart import Quart
+from quart_schema import QuartSchema
+from common.grpc_client.grpc_client import grpc_stream
+from common.repository.cyoda.cyoda_init import init_cyoda
+from app_init.app_init import cyoda_token
+#please update this line to your entity
+from entity.ENTITY_NAME_VAR.api import api_bp_ENTITY_NAME_VAR
+
+logging.basicConfig(level=logging.INFO)
+
+app = Quart(__name__)
+QuartSchema(app)
+app.register_blueprint(api_bp_ENTITY_NAME_VAR, url_prefix='/api/ENTITY_NAME_VAR')
+
+@app.before_serving
+async def startup():
+    await init_cyoda(cyoda_token)
+    app.background_task = asyncio.create_task(grpc_stream(cyoda_token))
+
+
+@app.after_serving
+async def shutdown():
+    app.background_task.cancel()
+    await app.background_task
 
 #put_application_code_here
+
+if __name__ == '__main__':
+    app.run(use_reloader=False, debug=True, host='0.0.0.0', port=8000, threaded=True)
