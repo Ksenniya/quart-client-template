f9404a83233933d4d849cd6cbd68405d12d81212
Ksenniya <lukkonina1@gmail.com>
2025-03-24 13:45:43 -0300

    saved entity/prototype.py: b8a73c88-08ce-11f0-a044-40c2ba0ac9eb

--- entity/prototype.py
+++ entity/prototype.py
@@ -13,12 +13,14 @@ logger.setLevel(logging.INFO)
 @dataclass
 class HelloRequest:
     name: str
+    surname: str  # Added surname parameter
 
 @app.route('/hello', methods=['GET'])
 @validate_querystring(HelloRequest)  # Workaround: validation placed first for GET requests
 async def hello():
     name = request.args.get('name', 'World')
-    return jsonify(message=f"Hello, {name}!")
+    surname = request.args.get('surname', '')  # Added surname extraction
+    return jsonify(message=f"Hello, {name} {surname}!")
 
 if __name__ == '__main__':
     app.run(use_reloader=False, debug=True, host='0.0.0.0', port=8000, threaded=True)
